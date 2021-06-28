912a6fa60e05403a060fa64bc08abc6ceecb5ac6
HM-L-1996 <dlgusals598@gmail.com>
2021-06-28 14:58:47 +0900

    pragmatic bugfix_0.2

--- accountapp/views.py
+++ accountapp/views.py
@@ -16,7 +16,7 @@ has_ownership = [account_ownership_required,login_required]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')  # class용 reverse 함수
+    success_url = reverse_lazy('accountapp:login')  # class용 reverse 함수
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView,MultipleObjectMixin):
@@ -36,7 +36,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:list')  # class용 reverse 함수
+    success_url = reverse_lazy('accountapp:detail')  # class용 reverse 함수
     template_name = 'accountapp/update.html'
 
 @method_decorator(has_ownership, 'get')
